6337c8d75a32d7ed3237bc2f65b0d4bb00b8883f
Matheus Leal <matheussleal98@gmail.com>
2024-05-23 15:27:23 -0300

    fix: restaurant list props

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -23,7 +23,6 @@ const RestaurantList = async () => {
         <RestaurantItem
           key={restaurant.id}
           restaurant={restaurant}
-          userId={session?.user?.id}
           userFavoriteRestaurants={userFavoriteRestaurants}
         />
       ))}
